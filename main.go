c1f799f906e0eb20e66a87000304e6836a395f76
Ian Bolton <ibolton@redhat.com>
2020-01-17 11:38:38 -0500

    add deck type and receiver function

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Sic of spaced")
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 	fmt.Println(cards)
 
 }

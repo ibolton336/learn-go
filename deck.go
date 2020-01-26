50d77b68f2724a4f0dc0eb23a780445f4531417c
Ian Bolton <ibolton@redhat.com>
2020-01-26 12:06:46 -0500

    convertion to byte slice

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
 
 // create a new type of deck which is a slice of string
 
@@ -31,6 +35,8 @@ func deal(d deck, handsize int) (deck, deck) {
 	return d[:handsize], d[handsize:]
 }
 
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 
-
+}
 
--- main.go
+++ main.go
@@ -4,13 +4,6 @@ import "fmt"
 
 
 func main() {
-	// cards := newDeck()
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print() 
-	// remainingCards.print()
-
-	//type conversion!
-	greeting := "HI there"
-	fmt.Println([]byte(greeting))
+	cards := newDeck()
+	fmt.Println(cards.toString())
 }
